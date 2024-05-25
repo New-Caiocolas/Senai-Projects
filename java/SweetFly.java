5d884c5fdc20a1693d228c0b8608b17cc1e005fb
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-05-24 23:45:34 -0300

    Update SweetFly.java

--- java/SweetFly.java
+++ java/SweetFly.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 
 public class SweetFly{
+
     public static void main(String[] args){
+        // Escaner para leitura de dados.
         Scanner s = new Scanner (System.in);
         int opcoes = 0;
+        // Vetor com registro do número de cada avião.
+        int[] aviaoNum = new int[4];
+
+        // Loop para quando for digitado o número 6 encerre.
         while (opcoes != 6){
             System.out.print("\n[1] Registrar o número de cada avião.\n[2] Registrar o quantitativo de assentos disponíveis em cada avião.\n[3] Reservar passagem aérea.\n[4] Realizar consulta por avião.\n[5] Realizar consulta por passageiro.\n[6] Encerrar.\nO que deseja fazer:");
             opcoes = s.nextInt();
 
+            // Opção de registrar o número do avião.
             if (opcoes == 1){
-                System.out.print("Digite o número do avião:");
+                for (int i = 0; i < aviaoNum.length; i++){
+                    System.out.print("Digite o número do avião:"); 
+                    aviaoNum[i] = s.nextInt();
+                }
             }
 
+            // Registrar a quantidade de assentos disponíveis em cada avião.
+            if (opcoes == 2){
+
+            }
+            
         }
 
     }
