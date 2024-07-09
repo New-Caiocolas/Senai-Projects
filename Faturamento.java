9e8dc29c1ad72b69c45d0abb81fb423021d3139b
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-07-09 17:25:06 -0300

    add

---
+++ Faturamento.java
@@ -0,0 +1,6 @@
+public class Faturamento {
+    public static void main(String[]args){
+        new tela();
+
+    }
+}
---
+++ tela.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class tela {
+    public tela(){
+        JFrame jf = new JFrame();
+        jf.setSize(800,500);
+        jf.setTitle("Faturamento");
+        jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+        jf.setResizable(false);
+        jf.setVisible(true);
+
+        
+    }
+}
