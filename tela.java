9eddeda18b6f8fed22b000ede3ca9cee8fcc3ae8
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-07-10 17:26:35 -0300

    Update tela.java

--- tela.java
+++ tela.java
@@ -1,14 +1,27 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class tela {
+
+
+public class tela extends JPanel{
+
     public tela(){
         JFrame jf = new JFrame();
         jf.setSize(800,500);
         jf.setTitle("Faturamento");
         jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
         jf.setResizable(false);
-        jf.setVisible(true);
+        jf.setBackground(new Color(9,9,9));
 
+        JLabel text = new JLabel("Digite aqui:");
+        text.setBounds(100, 100, 100, 100);
+        text.setFont(new Font("Arial", Font.BOLD, 15));
+
+
+        jf.add(text);
+        jf.setVisible(true);
         
     }
 }
